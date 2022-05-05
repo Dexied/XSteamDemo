591d7a7c190ebdb419abd6e7227afc247f0ee93c
Dexied <dexied.gs@gmail.com>
2022-05-06 05:18:22 +0600

    427-001

---
+++ Source/XSteamDemo/XSteamDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XSteamDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XSteamDemo, "XSteamDemo" );
