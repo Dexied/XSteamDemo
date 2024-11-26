c69d301319e857e0f94221af176809a53c034284
Dexied <33525453+Dexied@users.noreply.github.com>
2024-11-27 04:32:33 +0500

    550-000

--- Source/XSteamDemo.Target.cs
+++ Source/XSteamDemo.Target.cs
@@ -8,7 +8,7 @@ public class XSteamDemoTarget : TargetRules
 	public XSteamDemoTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "XSteamDemo" } );
 	}
 }
